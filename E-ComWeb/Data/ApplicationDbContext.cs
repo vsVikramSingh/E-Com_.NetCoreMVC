abd746f4319be23f74649512ffc823d9cefea1a8
vsVikramSingh <vsvikramsingh5200@gmail.com>
2024-01-30 11:02:07 +0530

    add constructor for DbContext

---
+++ E-ComWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace E_ComWeb.Data
+{
+    public class ApplicationDbContext : DbContext  
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
