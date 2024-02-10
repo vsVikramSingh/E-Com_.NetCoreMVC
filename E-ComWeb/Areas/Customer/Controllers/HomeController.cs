25f31232c732a90b55cd0991681e0c3d2f17bc26
vsVikramSingh <vsvikramsingh5200@gmail.com>
2024-02-10 21:29:16 +0530

    add details action

--- E-ComWeb/Areas/Customer/Controllers/HomeController.cs
+++ E-ComWeb/Areas/Customer/Controllers/HomeController.cs
@@ -24,6 +24,13 @@ namespace E_ComWeb.Areas.Customer.Controllers
             return View(productList);
         }
 
+        public IActionResult Details(int id)
+        {
+            Product product = _unitOfWork.Product.Get(u => u.ProductId == id, includeProperties: "Category");
+            return View(product);
+        }
+
+
         public IActionResult Privacy()
         {
             return View();
