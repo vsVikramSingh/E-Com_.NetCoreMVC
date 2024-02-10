0ed012649a2e03229c62ef8c0b590fa6dab59dc9
vsVikramSingh <vsvikramsingh5200@gmail.com>
2024-02-10 20:53:27 +0530

    update index action

--- E-ComWeb/Areas/Customer/Controllers/HomeController.cs
+++ E-ComWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 using E_ComWeb.Models;
+using ECom.DataAccess.Repository.IRepository;
+using ECom.Models;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
 
@@ -8,15 +10,18 @@ namespace E_ComWeb.Areas.Customer.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly IUnitOfWork _unitOfWork;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, IUnitOfWork unitOfWork)
         {
             _logger = logger;
+            _unitOfWork = unitOfWork;
         }
 
         public IActionResult Index()
         {
-            return View();
+            IEnumerable<Product> productList = _unitOfWork.Product.GetAll(includeProperties: "Category");
+            return View(productList);
         }
 
         public IActionResult Privacy()
