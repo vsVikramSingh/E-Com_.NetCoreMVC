eaa2bc3494b00a584074f8963f899729b0e0ec8b
vsVikramSingh <vsvikramsingh5200@gmail.com>
2024-02-10 20:51:27 +0530

    update repository

--- ECom.DataAccess/Repository/IRepository/IRepository.cs
+++ ECom.DataAccess/Repository/IRepository/IRepository.cs
@@ -6,8 +6,8 @@ namespace ECom.DataAccess.Repositotry.IRepository
     public interface IRepository<T> where T : class
     {
         // T - Category
-        IEnumerable<T> GetAll();
-        T Get(Expression<Func<T, bool>> filter);
+        IEnumerable<T> GetAll(string? includeProperties = null);
+        T Get(Expression<Func<T, bool>> filter, string? includeProperties = null);
         void Add(T entity);
         void Remove(T entity);
         void RemoveRange(IEnumerable<T> entity);
