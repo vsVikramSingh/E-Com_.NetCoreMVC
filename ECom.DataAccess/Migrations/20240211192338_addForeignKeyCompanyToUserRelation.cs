e9bbdf4bc9701e54378bb54d37e6ecad3ac26e3c
vsVikramSingh <vsvikramsingh5200@gmail.com>
2024-02-12 00:58:16 +0530

    add migration foreignkey campany to users

---
+++ ECom.DataAccess/Migrations/20240211192338_addForeignKeyCompanyToUserRelation.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace E_ComWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class addForeignKeyCompanyToUserRelation : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "CampanyId",
+                table: "AspNetUsers",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_CampanyId",
+                table: "AspNetUsers",
+                column: "CampanyId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Companies_CampanyId",
+                table: "AspNetUsers",
+                column: "CampanyId",
+                principalTable: "Companies",
+                principalColumn: "CompanyId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Companies_CampanyId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_CampanyId",
+                table: "AspNetUsers");
+
+            migrationBuilder.DropColumn(
+                name: "CampanyId",
+                table: "AspNetUsers");
+        }
+    }
+}
