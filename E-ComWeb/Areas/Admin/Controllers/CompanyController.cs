5351198ec2ac6c4e28d29a6189f89084ec0c4913
vsVikramSingh <vsvikramsingh5200@gmail.com>
2024-02-12 00:37:45 +0530

    add controller for company

---
+++ E-ComWeb/Areas/Admin/Controllers/CompanyController.cs
@@ -0,0 +1,102 @@
+﻿using ECom.DataAccess.Repository.IRepository;
+using ECom.Models;
+using ECom.Models.ViewModels;
+using ECom.Utility;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace E_ComWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    [Authorize(Roles = SD.Role_Admin)]
+    public class CompanyController : Controller
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+        public CompanyController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            List<Company> objCompany = _unitOfWork.Company.GetAll().ToList();
+
+            return View(objCompany);
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                // create
+                return View(new Company());
+            }
+            else
+            {
+                // update
+                Company company = _unitOfWork.Company.Get(u => u.CompanyId == id);
+                return View(company);
+            }
+
+        }
+
+        [HttpPost]
+        public IActionResult Upsert(Company company)
+        {
+            // validate state
+            if (ModelState.IsValid)
+            {
+                // check for create or update
+                if (company.CompanyId == 0)
+                {
+                    _unitOfWork.Company.Add(company);
+                    _unitOfWork.Save();
+                    TempData["success"] = "Company created successfully";
+                }
+                else
+                {
+                    _unitOfWork.Company.Update(company);
+                    _unitOfWork.Save();
+                    TempData["success"] = "Company updated successfully";
+                }
+
+                //_unitOfWork.Company.Add(productVM.Company);
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(company);
+            }
+        }
+
+
+        #region API CALLS
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Company> objCompany = _unitOfWork.Company.GetAll().ToList();
+            return Json(new { data = objCompany });
+        }
+
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            var companyToBeDeleted = _unitOfWork.Company.Get(u => u.CompanyId == id);
+
+            if (companyToBeDeleted == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+
+            _unitOfWork.Company.Remove(companyToBeDeleted);
+            _unitOfWork.Save();
+
+            return Json(new { success = true, message = "Delete Successful" });
+        }
+
+        #endregion
+    }
+}
