6dafbb27ebd83c9287acd7567efb1af938d989f4
vsVikramSingh <vsvikramsingh5200@gmail.com>
2024-02-12 01:47:33 +0530

    alter colname in users table

---
+++ ECom.DataAccess/Migrations/20240211200729_updateColnameInUsersTable.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace E_ComWeb.Migrations
+{
+    /// <inheritdoc />
+    public partial class updateColnameInUsersTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Companies_CampanyId",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "CampanyId",
+                table: "AspNetUsers",
+                newName: "CompanyId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_AspNetUsers_CampanyId",
+                table: "AspNetUsers",
+                newName: "IX_AspNetUsers_CompanyId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Companies_CompanyId",
+                table: "AspNetUsers",
+                column: "CompanyId",
+                principalTable: "Companies",
+                principalColumn: "CompanyId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_AspNetUsers_Companies_CompanyId",
+                table: "AspNetUsers");
+
+            migrationBuilder.RenameColumn(
+                name: "CompanyId",
+                table: "AspNetUsers",
+                newName: "CampanyId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_AspNetUsers_CompanyId",
+                table: "AspNetUsers",
+                newName: "IX_AspNetUsers_CampanyId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_AspNetUsers_Companies_CampanyId",
+                table: "AspNetUsers",
+                column: "CampanyId",
+                principalTable: "Companies",
+                principalColumn: "CompanyId");
+        }
+    }
+}
