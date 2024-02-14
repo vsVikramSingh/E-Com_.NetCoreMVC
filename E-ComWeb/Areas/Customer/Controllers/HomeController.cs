b3560d3e1d722a9f01b3242c92ce8fdb4abbba52
vsVikramSingh <vsvikramsingh5200@gmail.com>
2024-02-14 20:13:29 +0530

    update home controller for shoppingcart

--- E-ComWeb/Areas/Customer/Controllers/HomeController.cs
+++ E-ComWeb/Areas/Customer/Controllers/HomeController.cs
@@ -1,8 +1,10 @@
 using E_ComWeb.Models;
 using ECom.DataAccess.Repository.IRepository;
 using ECom.Models;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using System.Diagnostics;
+using System.Security.Claims;
 
 namespace E_ComWeb.Areas.Customer.Controllers
 {
@@ -26,10 +28,40 @@ namespace E_ComWeb.Areas.Customer.Controllers
 
         public IActionResult Details(int id)
         {
-            Product product = _unitOfWork.Product.Get(u => u.ProductId == id, includeProperties: "Category");
-            return View(product);
+            ShoppingCart cart = new()
+            {
+                Product = _unitOfWork.Product.Get(u => u.ProductId == id, includeProperties: "Category"),
+                Count = 1,
+                ProductId = id
+            };
+            return View(cart);
         }
 
+        [HttpPost]
+        [Authorize]
+        public IActionResult Details(ShoppingCart shoppingCart)
+        {
+            var claimsIdentity = (ClaimsIdentity)User.Identity;
+            var userId = claimsIdentity.FindFirst(ClaimTypes.NameIdentifier).Value;
+            shoppingCart.ApplicationUserId = userId;
+
+            ShoppingCart cartFromDb = _unitOfWork.ShoppingCart.Get(u=>u.ApplicationUserId==userId && u.ProductId==shoppingCart.ProductId);
+            
+            
+            if(cartFromDb != null)
+            {
+                cartFromDb.Count += shoppingCart.Count;
+                _unitOfWork.ShoppingCart.Update(cartFromDb);
+            }
+            else
+            {
+                _unitOfWork.ShoppingCart.Add(shoppingCart);
+            }
+
+            TempData["success"] = "Cart updated successfully";
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
 
         public IActionResult Privacy()
         {
