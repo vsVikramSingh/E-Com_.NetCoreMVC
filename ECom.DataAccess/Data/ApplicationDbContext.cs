6f56089777acad7d4d5a19189bb0d78cd05dd513
vsVikramSingh <vsvikramsingh5200@gmail.com>
2024-02-14 20:11:23 +0530

    update dbcontext for shoppingcart

--- ECom.DataAccess/Data/ApplicationDbContext.cs
+++ ECom.DataAccess/Data/ApplicationDbContext.cs
@@ -18,6 +18,7 @@ namespace E_ComWeb.Data
         public DbSet<Product> Products { get; set; }
         public DbSet<Company> Companies { get; set; }
         public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+        public DbSet<ShoppingCart> ShoppingCarts { get; set; }
 
     }
 }
