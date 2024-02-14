81736f100247305c46a2220dde6216c8f28640f5
vsVikramSingh <vsvikramsingh5200@gmail.com>
2024-02-14 23:25:30 +0530

    update cart controller

--- E-ComWeb/Areas/Customer/Controllers/CartController.cs
+++ E-ComWeb/Areas/Customer/Controllers/CartController.cs
@@ -35,6 +35,46 @@ namespace E_ComWeb.Areas.Customer.Controllers
             return View(shoppingCartVM);
         }
 
+        public ActionResult Summary()
+        {
+            return View();
+        }
+
+        public IActionResult Plus(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u=>u.ProductId==cartId);
+            cartFromDb.Count += 1;
+            _unitOfWork.ShoppingCart.Update(cartFromDb);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Minus(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.ProductId == cartId);
+            if (cartFromDb.Count <= 1)
+            {
+                // remove that from cart
+                _unitOfWork.ShoppingCart.Remove(cartFromDb);
+            }
+            else
+            {
+                cartFromDb.Count -= 1;
+                _unitOfWork.ShoppingCart.Update(cartFromDb);
+            }
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+
+        public IActionResult Remove(int cartId)
+        {
+            var cartFromDb = _unitOfWork.ShoppingCart.Get(u => u.ProductId == cartId);
+            _unitOfWork.ShoppingCart.Remove(cartFromDb);
+            _unitOfWork.Save();
+            return RedirectToAction(nameof(Index));
+        }
+            
+
         private double GetPriceBasedOnQuantity(ShoppingCart shoppingCart)
         {
             if(shoppingCart.Count <= 50)
