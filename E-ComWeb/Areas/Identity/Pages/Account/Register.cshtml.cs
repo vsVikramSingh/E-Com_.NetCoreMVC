4b8e8b48023b636bd307c0c627c661fa1c045684
vsVikramSingh <vsvikramsingh5200@gmail.com>
2024-02-18 19:28:57 +0530

    update code for only admin can register for employee and company role

--- E-ComWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ E-ComWeb/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -208,7 +208,14 @@ namespace E_ComWeb.Areas.Identity.Pages.Account
                     }
                     else
                     {
-                        await _signInManager.SignInAsync(user, isPersistent: false);
+                        if (User.IsInRole(SD.Role_Admin))
+                        {
+                            TempData["success"] = "New User Created successfully";
+                        }
+                        else
+                        {
+                            await _signInManager.SignInAsync(user, isPersistent: false);
+                        }
                         return LocalRedirect(returnUrl);
                     }
                 }
