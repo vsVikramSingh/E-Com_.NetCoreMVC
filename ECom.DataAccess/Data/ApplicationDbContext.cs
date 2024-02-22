9a12927773381c8bc6cdc33926aca0884eec3c29
vsVikramSingh <vsvikramsingh5200@gmail.com>
2024-02-22 15:31:22 +0530

    update migration and images

--- ECom.DataAccess/Data/ApplicationDbContext.cs
+++ ECom.DataAccess/Data/ApplicationDbContext.cs
@@ -21,5 +21,6 @@ namespace E_ComWeb.Data
         public DbSet<ShoppingCart> ShoppingCarts { get; set; }
         public DbSet<OrderHeader> OrderHeaders { get; set; }
         public DbSet<OrderDetail> OrderDetails { get; set; }
+        public DbSet<ProductImage> ProductImages { get; set; }
     }
 }
--- ECom.DataAccess/Migrations/20240222083705_addProductImageToDb.Designer.cs
+++ ECom.DataAccess/Migrations/20240222083705_addProductImageToDb.Designer.cs
@@ -4,6 +4,7 @@ using E_ComWeb.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace ECom.DataAccess.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20240222083705_addProductImageToDb")]
+    partial class addProductImageToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
